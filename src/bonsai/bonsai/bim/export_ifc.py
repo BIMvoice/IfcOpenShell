52eb3b7a47a6a9fd39fcb6c82d44d273327453b6
Petru Conduraru <petru@bimvoice.com>
2026-08-18 13:37:19 +0300

    Bonsai: link external SVG/DXF files as snappable reference geometry

--- src/bonsai/bonsai/bim/export_ifc.py
+++ src/bonsai/bonsai/bim/export_ifc.py
@@ -44,6 +44,7 @@ class IfcExporter:
         self.file = tool.Ifc.get()
         self.set_header()
         self.sync_all_objects()
+        tool.LinkedReference.sync_placements_to_ifc()
         extension = self.ifc_export_settings.output_file.split(".")[-1].lower()
         if extension == "ifczip":
             with tempfile.TemporaryDirectory() as unzipped_path:
